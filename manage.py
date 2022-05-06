d5b6c1e749f0e6cb27f975f8955c496bbff70609
tossode <tossdev@189.cn>
2022-05-06 17:27:01 +0800

    tossode <82136882+tossode@users.noreply.github.com>

--- manage.py
+++ manage.py
@@ -1,5 +1,10 @@
 from config.application import create_app
+from lib.tasks.database import database_cli
+from lib.tasks.graphql import graphql_cli
+
 app = create_app()
+app.cli.add_command(graphql_cli)
+app.cli.add_command(database_cli)
 
 if __name__ == '__main__':
   app.run()
